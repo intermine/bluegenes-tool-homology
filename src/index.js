92d63162f4102c1495f0f344a7af5f8846147a7a
uosl <regen@anche.no>
2019-06-06 09:54:10 +0100

    Restructure code to include instance data with homologues

--- src/index.js
+++ src/index.js
@@ -1,58 +1,3 @@
-function neighbourUrls(data) {
-  var obj = {};
-
-  data.instances.forEach(function(instance) {
-    instance.neighbours.forEach(function(neighbour) {
-      if (obj[neighbour]) {
-        obj[neighbour].push(instance.url);
-      } else {
-        obj[neighbour] = [instance.url];
-      }
-    });
-  });
-
-  return obj;
-}
-
-function queryHomologues(symbol, url) {
-  return new Promise(function(resolve) {
-    var intermine = new imjs.Service({root: url});
-
-    var query = {
-      "from": "Gene",
-      "select": [
-        "secondaryIdentifier",
-        "symbol",
-        "primaryIdentifier",
-        "organism.name",
-        "homologues.homologue.secondaryIdentifier",
-        "homologues.homologue.symbol",
-        "homologues.homologue.primaryIdentifier",
-        "homologues.homologue.organism.name"
-      ],
-      "orderBy": [
-        {
-          "path": "secondaryIdentifier",
-          "direction": "ASC"
-        }
-      ],
-      "where": [
-        {
-          "path": "homologues.homologue",
-          "op": "LOOKUP",
-          "value": symbol,
-          "extraValue": "",
-          "code": "A"
-        }
-      ]
-    };
-
-    intermine.records(query).then(function(res) {
-      resolve([url, res]);
-    });
-  });
-}
-
 function querySymbol(url, id) {
   return new Promise(function(resolve) {
     var intermine = new imjs.Service({root: url});
@@ -87,44 +32,6 @@ function querySymbol(url, id) {
   });
 }
 
-function geneToSymbol(obj) {
-  return obj.symbol || obj.primaryIdentifier || obj.secondaryIdentifier;
-}
-
-function extractHomologueLists(queryResults) {
-  var obj = {}
-
-  queryResults.forEach(function(result) {
-    var url = result[0];
-    var homologueList = result[1];
-    obj[url] = homologueList.map(geneToSymbol);
-  })
-
-  return obj;
-}
-
-function createPortalUrl(gene, apiUrl) {
-  return apiUrl.concat(
-    "/portal.do",
-    "?class=Gene",
-    "&externalid=".concat(gene)
-  );
-}
-
-function renderHomologueLinks(homologueObj, node) {
-  Object.keys(homologueObj).forEach(function(url) {
-    var genes = homologueObj[url];
-
-    genes.forEach(function(gene) {
-      var anchor = document.createElement("a");
-      anchor.href = createPortalUrl(gene, url);
-      var text = document.createTextNode(gene);
-      anchor.appendChild(text);
-      node.appendChild(anchor);
-    })
-  })
-}
-
 function queryNeighbours(mineUrl) {
   return new Promise(function(resolve) {
     fetch(
@@ -147,23 +54,98 @@ function queryNeighbours(mineUrl) {
   });
 }
 
+function queryHomologues(symbol, instance) {
+  return new Promise(function(resolve) {
+    var intermine = new imjs.Service({root: instance.url});
+
+    var query = {
+      "from": "Gene",
+      "select": [
+        "secondaryIdentifier",
+        "symbol",
+        "primaryIdentifier",
+        "organism.name",
+        "homologues.homologue.secondaryIdentifier",
+        "homologues.homologue.symbol",
+        "homologues.homologue.primaryIdentifier",
+        "homologues.homologue.organism.name"
+      ],
+      "orderBy": [
+        {
+          "path": "secondaryIdentifier",
+          "direction": "ASC"
+        }
+      ],
+      "where": [
+        {
+          "path": "homologues.homologue",
+          "op": "LOOKUP",
+          "value": symbol,
+          "extraValue": "",
+          "code": "A"
+        }
+      ]
+    };
+
+    intermine.records(query).then(function(res) {
+      resolve([instance, res]);
+    });
+  });
+}
+
+function geneToSymbol(obj) {
+  return obj.symbol || obj.primaryIdentifier || obj.secondaryIdentifier;
+}
+
+function createPortalUrl(apiUrl, gene) {
+  return apiUrl.concat(
+    "/portal.do",
+    "?class=Gene",
+    "&externalid=".concat(gene)
+  );
+}
+
+function renderHomologueLinks(node, results) {
+  results.forEach(function(result) {
+    var instance = result[0];
+    var homologueList = result[1];
+
+    var div = document.createElement("div");
+
+    var mine = document.createElement("span");
+    mine.appendChild(document.createTextNode(instance.name));
+    div.appendChild(mine);
+
+    homologueList.forEach(function(homologue) {
+      var gene = geneToSymbol(homologue);
+
+      var anchor = document.createElement("a");
+      anchor.href = createPortalUrl(instance.url, gene);
+      anchor.appendChild(document.createTextNode(gene));
+      div.appendChild(anchor);
+    })
+
+    node.appendChild(div);
+  })
+}
+
 export function main (el, service, imEntity, state, config) {
-  querySymbol(service.root, imEntity.value).then(function(symbol) {
+  querySymbol(service.root, imEntity.value).then(function(targetSymbol) {
     queryNeighbours(service.root).then(function(neighbours) {
-      neighbours.forEach(function(family) {
+      neighbours.forEach(function(targetNeighbour) {
         fetch("https://registry.intermine.org/service/instances")
           .then(function(res) { return res.json(); })
-          .then(function(instancesData) {
-            var familyToUrls = neighbourUrls(instancesData);
-            var urls = familyToUrls[family];
+          .then(function(data) {
+            var instances = data.instances.filter(function(instance) {
+              return instance.neighbours.includes(targetNeighbour);
+            });
 
-            var homologuePromises = urls.map(function(url) {
-              return queryHomologues(symbol, url);
+            var homologuePromises = instances.map(function(instance) {
+              return queryHomologues(targetSymbol, instance);
             });
 
             Promise.all(homologuePromises).then(function(results) {
-              var homologueObj = extractHomologueLists(results);
-              renderHomologueLinks(homologueObj, el);
+              renderHomologueLinks(el, results);
             });
           });
       });
